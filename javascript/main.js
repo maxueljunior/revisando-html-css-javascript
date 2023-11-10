5727de25fb50e172d988bbd6ef72bb8d81aaa648
Maxuel Vieira Tobá Junior <maxueljunior@weg.net>
2023-11-10 16:55:45 -0300

    aula dia 10/11 - almoço

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,13 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementAudio){
+    document.querySelector(idElementAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas.forEach(x => {
+    const classe = x.classList[1];
+    const audio = `#som_${classe}`;
+    x.onclick = function() {
+        tocaSom(audio)
+    };
+});
