d16846928cc25bdd6ef2b403ba708fcf81c7a6a6
Maxuel Vieira Tobá Junior <maxueltstz@hotmail.com>
2023-11-11 18:14:54 -0300

    aula dia 11/11 - parte 1

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,11 @@
 function tocaSom(idElementAudio){
-    document.querySelector(idElementAudio).play();
+    const elemento = document.querySelector(idElementAudio);
+
+    if(elemento != null ){
+        if(elemento.localName === 'audio'){
+            elemento.play();
+        }
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -7,7 +13,20 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 listaDeTeclas.forEach(x => {
     const classe = x.classList[1];
     const audio = `#som_${classe}`;
+
     x.onclick = function() {
         tocaSom(audio)
     };
+
+    x.onkeydown = function(evento) {
+6
+        if(evento.key == 'Enter' || evento.key == 'Space'){
+            x.classList.add('ativa');
+        }
+        
+    }
+    
+    x.onkeyup = function() {
+        x.classList.remove('ativa');
+    }
 });
