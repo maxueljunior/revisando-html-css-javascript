648baaf9b2d2960053d9283de86860008effe334
Maxuel Vieira Tobá Junior <maxueljunior@weg.net>
2023-11-16 17:01:46 -0300

    aula dia 16.11 - almoço

--- javascript 6/app/metodoFilter.js
+++ javascript 6/app/metodoFilter.js
@@ -5,6 +5,26 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros(){
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if(categoria == 'disponivel'){
+        const valorTotal = calcularValorTotal(livrosFiltrados)
+        exibirValorTotalDosLivros(valorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivros(valorTotal){
+    elementoComValorTotal.innerHTML = `
+    <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
 }
