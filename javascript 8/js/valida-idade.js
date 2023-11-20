102bfdd54df46fb4c79b8500bb8982ae70df88d8
Maxuel Vieira Tobá Junior <maxueltstz@hotmail.com>
2023-11-20 11:54:22 -0300

    finalizando javascript e inicado TypeScript

---
+++ javascript 8/js/valida-idade.js
@@ -0,0 +1,13 @@
+export default function ehMaiorDeIdade(campo){
+    const dataNascimento = new Date(campo.value)
+    if(!validaIdade(dataNascimento)){
+        campo.setCustomValidity('O usuario não e maior de idade')
+    }
+}
+
+function validaIdade(dataNascimento){
+    const dataAtual = new Date()
+    const dataMais18 = new Date(dataNascimento.getUTCFullYear() + 18, dataNascimento.getUTCMonth(), dataNascimento.getUTCDate())
+
+    return dataAtual >= dataMais18
+}
