f32fd18aebc3fb97ef924f4c24c75d461afa5345
Maxuel Vieira Tobá Junior <maxueljunior@weg.net>
2023-11-23 15:26:07 -0300

    aula dia 23.11 - almoço

--- typescript 1/dist/js/models/negociacoes.js
+++ typescript 1/dist/js/models/negociacoes.js
@@ -1,12 +1,10 @@
 export class Negociacoes {
     constructor() {
-        // private negociacoes: Array<Negociacao> = [];
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
-    // listar(): ReadonlyArray<Negociacao>{
     listar() {
         return this.negociacoes;
     }
